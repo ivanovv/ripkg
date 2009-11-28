2c7a73580b632941cc2d5304043263bcfe6f2997
ViC <vic@ubuntu.ubuntu-domain>
2009-11-28 04:03:32 +0300

    attempt 4 to get it running

--- lib/common.rb
+++ lib/common.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', '..')
+#$LOAD_PATH << File.join(File.dirname(__FILE__), '..', '..')
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
 require 'rubygems'
@@ -11,22 +11,6 @@ require 'ipkg'
 
 # Models
 require 'model'
-
-def plural(count, singular, plural)
-    count == 1 ? singular : plural
-end 
-
-class Fixnum
-  def to_filesize(precision = 1)
-      bytes = self
-      case 
-        when bytes < 1024; "%d #{plural(bytes, 'byte', 'bytes')}" % bytes
-        when bytes < 1048576; "%.#{precision}f #{plural((bytes / 1024), 'kilobyte', 'kilobytes')}" % (bytes / 1024)
-        when bytes < 1073741824; "%.#{precision}f #{plural((bytes / 1048576), 'megabyte', 'megabytes')}" % (bytes / 1048576)
-        when bytes >= 1073741824; "%.#{precision}f #{plural((bytes / 1073741824), 'gigabyte', 'gigabytes')}" % (bytes / 1073741824)
-      end
-  end
-end
 
 module Sinatra
   module Hat
@@ -61,4 +45,21 @@ module Sinatra
     end
   end
 end
-    
+
+
+def plural(count, singular, plural)
+    count == 1 ? singular : plural
+end 
+
+class Fixnum
+  def to_filesize(precision = 1)
+      bytes = self
+      case 
+        when bytes < 1024; "%d #{plural(bytes, 'byte', 'bytes')}" % bytes
+        when bytes < 1048576; "%.#{precision}f #{plural((bytes / 1024), 'kilobyte', 'kilobytes')}" % (bytes / 1024)
+        when bytes < 1073741824; "%.#{precision}f #{plural((bytes / 1048576), 'megabyte', 'megabytes')}" % (bytes / 1048576)
+        when bytes >= 1073741824; "%.#{precision}f #{plural((bytes / 1073741824), 'gigabyte', 'gigabytes')}" % (bytes / 1073741824)
+      end
+  end
+end
+
