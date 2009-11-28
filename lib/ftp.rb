b095d5c2c22167463b353bbaf3690db7fea44c1e
ViC <vic@ubuntu.ubuntu-domain>
2009-11-28 17:01:55 +0300

    moved constants from method to class declaration

--- lib/ftp.rb
+++ lib/ftp.rb
@@ -1,20 +1,20 @@
 require 'net/ftp'
 
 class RouterDownload
+  
+  ROUTER_NAME = 'myrouter.homelinux.net'
+  ROUTER_DIR = '/ipkg'
+  ROUTER_LIST_DIR = 'lists'
+  LOCAL_TMP_DIR = './tmp'
+  STATUS_FILE = 'status'
 
   def self.do_the_job
-    ROUTER_NAME = 'myrouter.homelinux.net'
-    ROUTER_DIR = '/ipkg'
-    ROUTER_LIST_DIR = 'lists'
-    LOCAL_TMP_DIR = './tmp'
-    STATUS_FILE = 'status'
-
+   
     ftp = Net::FTP.new(ROUTER_NAME)
     ftp.passive = true
     ftp.login
     ftp.chdir(ROUTER_DIR)
     ftp.gettextfile(STATUS_FILE, File.join(LOCAL_TMP_DIR, STATUS_FILE))
-
     ftp.chdir(ROUTER_LIST_DIR)
 
     local_dir = File.join(LOCAL_TMP_DIR, ROUTER_LIST_DIR)
@@ -24,7 +24,6 @@ class RouterDownload
       local_file = File.join(local_dir, ftp_file)
       File.delete (local_file) if File.exists?(local_file)
       ftp.gettextfile(ftp_file, local_file)
-    end
-    Thread.exit
+    end    
   end
 end
