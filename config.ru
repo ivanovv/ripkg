c6e7031a665703f68ec873bead33343cb61e24a3
ViC <vic@ubuntu.ubuntu-domain>
2010-02-17 21:20:33 +0300

    changed parsing a bit

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'app.rb')
+require ::File::join(::File::dirname(__FILE__), 'app.rb')
 
 disable :run
 
--- list_parser.rb
+++ list_parser.rb
@@ -3,19 +3,24 @@ require 'model'
 
 class ListParser
 
-  def initialize (lists_path, record_delimiter)
-    @lists_path = lists_path
+  def initialize (list_path, record_delimiter)
+    @list_path = list_path
     @record_delimiter = record_delimiter
   end
 
-  def parse    
-    Dir[@lists_path + "/*"].each do  |list_path| 
+  def parse
+    if File.directory? @list_path
+      files_to_parse = Dir[@list_path + "/*"]
+    else
+      files_to_parse = []; files_to_parse << @list_path
+    end
+    files_to_parse.each do |list_path|
       if !File.directory?(list_path) then
         File.open(list_path).each(@record_delimiter) do |package|
           package.strip!
           yield(package) if package.length > 0
         end
-      end      
+      end
     end
   end
     
